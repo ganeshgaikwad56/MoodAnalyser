e038e6bbf9ac2ec0b0763dcb8054cd4bb7fda7d8
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-12 21:52:58 +0530

    Ganesh Adding Program file for UC1

---
+++ Operation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    public class Operation
+    {
+
+        /// <summary>
+        /// Moods the analyze.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <returns></returns>
+        /// 
+
+        public string MoodAnalyze(string message)
+        {
+            return "HAPPY";
+        }
+        /// <summary>
+        /// Checks the mood.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <returns></returns>
+        /// 
+
+
+        public string CheckMood(string message)
+        {
+            return "SAD";
+        }
+    }
+}
