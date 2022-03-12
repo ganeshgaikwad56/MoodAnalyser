5f562d65df5efc28a2c1fbf330c7f1f00b8902f1
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-12 21:44:15 +0530

    Ganesh Adding Greeting Message

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MoodAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Program.");
+
+            
+        }
+    }
+}
