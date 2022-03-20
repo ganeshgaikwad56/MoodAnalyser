8a15997ddcd563d68999ee09991bf11d32b0126d
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-20 16:18:53 +0530

    Ganesh adding program file for uc3

---
+++ CustomException.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.Serialization;
+
+namespace MoodAnalyser
+{
+    [Serializable]
+    internal class CustomException : Exception
+    {
+        private object nULL_EXCEPTION;
+        private string v;
+
+        public CustomException()
+        {
+        }
+
+        public CustomException(string? message) : base(message)
+        {
+        }
+
+        public CustomException(object nULL_EXCEPTION, string v)
+        {
+            this.nULL_EXCEPTION = nULL_EXCEPTION;
+            this.v = v;
+        }
+
+        public CustomException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected CustomException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public static object ExceptionType { get; internal set; }
+    }
+}
